8f104de937a3df27e5d00aee1f712b358bd13add
Arnout Engelen <arnout@bzzt.net>
2023-02-21 15:34:19 +0100

    create, cache and parse a scorecard

---
+++ scorecard.scala
@@ -0,0 +1,54 @@
+package scorecard
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.Promise
+import scala.concurrent.duration._
+import sys.process.Process
+import sys.process.ProcessIO
+
+import org.apache.commons.io.IOUtils
+
+import spray.json._
+import DefaultJsonProtocol._
+
+import cached.Cached
+
+case class Repo(name: String, commit: String)
+object Repo:
+  given JsonFormat[Repo] = jsonFormat2(Repo.apply)
+  
+case class Check(name: String, score: Int, reason: String, details: Option[Seq[String]])
+object Check:
+  given JsonFormat[Check] = jsonFormat4(Check.apply)
+
+case class Scorecard(repo: Repo, score: Double, checks: Seq[Check])
+object Scorecard:
+  given JsonReader[Scorecard] = jsonFormat3(Scorecard.apply)
+
+object Scorecards extends Cached[String](identity):
+  override def fetchUncached(repo: String): Array[Byte] =
+    var p = Promise[java.io.InputStream]
+    var e = Promise[java.io.InputStream]
+    var seq = Seq("scorecard", s"--repo=$repo", "--format=json", "--show-details")
+    var proc = Process(seq)
+    var process = proc.run(new ProcessIO(
+      _.close(),
+      (stdout: java.io.InputStream) => {
+        p.completeWith(Future.successful(stdout))
+      },
+      (stderr: java.io.InputStream) => {
+        e.completeWith(Future.successful(stderr))
+      },
+    ))
+    
+    given ExecutionContext = ExecutionContext.global
+    val ro = Await.result(p.future, 60.seconds)
+    val re = Await.result(e.future, 60.seconds)
+    println(process.exitValue())
+    val errors = IOUtils.toByteArray(re)
+    println("Errors: " + new String(errors))
+    val result = IOUtils.toByteArray(ro)
+    println("Output: " + new String(result))
+    result
